26e80ecbf89a429bafd0431e3acccf0a1f72ac7e
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-06-04 10:16:34 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,11 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 14:51:21 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/05/31 15:33:56 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/06/04 10:16:08 by event            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Libft.h"
-#include <string.h>
+#include "libft.h"
 
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -21,22 +20,22 @@ char		*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	int		j;
 	int		c;
 
-	i = -1;
+	i = 0;
 	c = 0;
 	s = (char *)haystack;
 	str = (char *)needle;
 	if (*str == '\0')
 		return (s);
-	while (s[++i] != '\0' && i < len)
+	while (s[i] != '\0' && i < len)
 	{
 		j = -1;
-		while (str[++j] != '\0')
+		while (str[++j] != '\0' && (i + j) < len)
 			if (str[j] == s[i + j])
 				c++;
 		if (c == (int)ft_strlen(str))
 			return (s + i);
-		else
-			c = 0;
+		i++;
+		c = 0;
 	}
 	return (NULL);
 }
