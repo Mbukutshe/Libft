2a72060470b26ae46097189b20bf3c7f587dc649
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-06-05 09:15:02 +0200

    Add files via upload

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:54:38 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/05/31 15:40:03 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/06/05 09:13:38 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,11 @@ int		ft_strcmp(const char *s1, const char *s2)
 		if (str1[i] > str2[i])
 			return (1);
 		else if (str1[i] < str2[i])
-			return (-1);
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 		else
 			result = 0;
 	}
+	if (str2[i] != '\0')
+		return (-1);
 	return (result);
 }
