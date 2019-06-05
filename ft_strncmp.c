56c99e2f8805c9f32e641265bddac9e19874d96a
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-06-05 10:38:06 +0200

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 16:45:09 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/06/04 10:34:13 by event            ###   ########.fr       */
+/*   Updated: 2019/06/05 10:35:15 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,9 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-	while (s1[i] && i < n)
-	{
-		if (s1[i] == '\0')
-			return (1);
-		else if (s1[i] != s2[i])
-		{
-			if (s1[i] > s2[i])
-				return (s1[i] - s2[i]);
-			else
-				return (s2[i] - s1[i]);
-		}
+	if (n == 0)
+		return (0);
+	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
 		i++;
-	}
-	if (s2[i] != '\0' && i != n)
-		return (-1);
-	return (0);
+	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
 }
