250d9cca31631c7d0f07ed9ff6a69fb7083c8d72
Mbukutshe <kmbukuts@student.wethinkcode.co.za>
2019-06-22 03:59:52 +0200

    Add files via upload

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:54:38 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/06/06 10:14:01 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/06/22 02:33:30 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,15 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	unsigned char	*str1;
-	unsigned char	*str2;
-	int				i;
-	int				result;
+	int i;
 
-	i = -1;
-	result = 0;
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
-	while (str1[++i])
-	{
-		if (str1[i] > str2[i])
-			return (1);
-		else if (str1[i] < str2[i])
-			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
-		else
-			result = 0;
-	}
-	if (str2[i] != '\0')
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		++i;
+	if ((unsigned char)s1[i] - (unsigned char)s2[i] > 0)
+		return (1);
+	else if ((unsigned char)s1[i] - (unsigned char)s2[i] < 0)
 		return (-1);
-	return (result);
+	else
+		return (0);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: kmbukuts <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 16:45:09 by kmbukuts          #+#    #+#             */
-/*   Updated: 2019/06/06 10:14:16 by kmbukuts         ###   ########.fr       */
+/*   Updated: 2019/06/22 02:06:57 by kmbukuts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,5 +25,10 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 		return (0);
 	while (str1[i] != '\0' && str1[i] == str2[i] && i < n - 1)
 		i++;
-	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+	if ((unsigned char)str1[i] - (unsigned char)str2[i] > 0)
+		return (1);
+	else if ((unsigned char)str1[i] - (unsigned char)str2[i] < 0)
+		return (-1);
+	else
+		return (0);
 }
